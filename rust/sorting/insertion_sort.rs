a8d16bd862915d90ca3a613a79933913af0733d3
Mark <frozenstranger510@gmail.com>
2025-04-14 20:49:51 -0500

    sorting: insertion sort for rust

--- rust/sorting/insertion_sort.rs
+++ rust/sorting/insertion_sort.rs
@@ -1,7 +1,29 @@
-// rand = "0.7"
+// rand = "0.9"
 
-use rand::Rng;
+use rand::prelude::*;
+
+// for the use of cargo-single we have to traverse backwards.
+include!{"../../random_value.rs"}
 
 fn main() {
-    println!("Hello, world!");
+    const ARRAY_SIZE : i32 = 5;
+    let mut array = randomize_array(1, 5, ARRAY_SIZE);
+
+    // Start with 2nd item:
+    for i in 1..array.len() {
+        let n = array[i];
+        let mut j = i;
+
+        // Check previous:
+        while j > 0 && array[j-1] > n {
+            array[j] = array[j-1];
+            dbg!(j);
+            j-=1;
+        }
+
+        // Insert front:
+        array[j] = n;
+    }
+
+    dbg!(array);
 }
