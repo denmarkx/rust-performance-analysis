23fb1c1d82b02c6c8447ce8cf464e22401e11d89
Mark <frozenstranger510@gmail.com>
2025-04-16 01:28:11 -0500

    rust: benchmark for insertion_sort.

--- rust/sorting/insertion_sort.rs
+++ rust/sorting/insertion_sort.rs
@@ -1,5 +1,6 @@
 // rand = "0.9"
 
+use std::time::Instant;
 use rand::prelude::*;
 
 // for the use of cargo-single we have to traverse backwards.
@@ -10,6 +11,7 @@ fn main() {
     let mut array = randomize_array(1, 5, ARRAY_SIZE);
 
     // Start with 2nd item:
+    let now = Instant::now();
     for i in 1..array.len() {
         let n = array[i];
         let mut j = i;
@@ -17,13 +19,12 @@ fn main() {
         // Check previous:
         while j > 0 && array[j-1] > n {
             array[j] = array[j-1];
-            dbg!(j);
             j-=1;
         }
 
         // Insert front:
         array[j] = n;
     }
-
-    dbg!(array);
+    let elapsed = now.elapsed();
+    println!("Time Elapsed: {:.6?}", elapsed);
 }
