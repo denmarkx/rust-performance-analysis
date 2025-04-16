0d0b8de896a2a60ba979a9bc2583925934b63654
Mark <frozenstranger510@gmail.com>
2025-04-16 17:55:41 -0500

    sorting_unsafe: benchmarking and args for insertion sort

--- rust/sorting_unsafe/insertion_sort.rs
+++ rust/sorting_unsafe/insertion_sort.rs
@@ -1,30 +1,38 @@
 // rand = "0.9"
 
+use std::env;
 use rand::prelude::*;
 
 // for the use of cargo-single we have to traverse backwards.
 include!{"../../random_value.rs"}
+include!{"../../../benchmark.rs"}
+include!{"../../../util.rs"}
 
+fn insertion_sort(array: &mut [u32]) {
+    // Start with 2nd item:
+    unsafe {
+        for i in 1..array.len() {
+            let n = *array.get_unchecked(i);
+            let mut j = i;
+
+            // Check previous:
+            while j > 0 && *array.get_unchecked(j-1) > n {
+                array[j] = *array.get_unchecked(j-1);
+                j-=1;
+            }
+
+            // Insert front:
+            array[j] = n;
+        }
+    }
+}
 
 fn main() {
-    const ARRAY_SIZE : i32 = 5;
-    let mut array = randomize_array(1, 5, ARRAY_SIZE);
+    let mut args : Vec<String> = env::args().collect();
+    warn_arguments(&mut args);
 
-    // Start with 2nd item:
-    for i in 1..array.len() {
-        let n = unsafe { *array.get_unchecked(i) };
-        let mut j = i;
+    let array_size : usize = args[1].parse::<usize>().unwrap();
+    let mut array = randomize_array(1, args[2].parse::<u32>().unwrap(), array_size);
 
-        // Check previous:
-        while j > 0 && unsafe { *array.get_unchecked(j-1) } > n {
-            array[j] = unsafe { *array.get_unchecked(j-1) };
-            dbg!(j);
-            j-=1;
-        }
-
-        // Insert front:
-        array[j] = n;
-    }
-
-    dbg!(array);
+    benchmark(args[3].parse::<usize>().unwrap(), || insertion_sort(&mut array));
 }
