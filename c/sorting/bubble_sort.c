0d6e5c937c2aa1396bb5b48bfcdafc654b0ee341
Mark <frozenstranger510@gmail.com>
2025-04-19 20:52:16 -0500

    sorting: extend random/file impl to bubble and quick sort

--- c/sorting/bubble_sort.c
+++ c/sorting/bubble_sort.c
@@ -2,7 +2,6 @@
 
 #include "random_value.c"
 #include "benchmark.c"
-#include "util.c"
 
 void bubble_sort(int* array, const int array_size) {
     benchmark();
@@ -21,20 +20,30 @@ void bubble_sort(int* array, const int array_size) {
 
 int main(int argc, char* argv[]) {
     warn_arguments(argc, argv);
-    setup_benchmark(atoi(argv[3]));
+    setup_benchmark(atoi(argv[2]));
 
-    const int array_size = atoi(argv[2]);
+    // Insertion Sort implementation:
+    int array_size;
+    int* array = NULL;
+    ArrayData arr_data;
+    arr_data.arr_size = 0;
 
-    int* array = random_value(0, atoi(argv[1]), array_size);
+    if (use_random_values(argv[1])) {
+        // array = random_value(0, atoi(argv[1]), array_size);
+        arr_data = random_value_set(0, atoi(argv[1]), atoi(argv[2]));
+    } else {
+        arr_data = read_from_file("../test/bubble_sort.txt");
+    }
 
-    // Bubble Sort implementation:
-    for (int i = 0; i < atoi(argv[3]); i++) {
+    // Traverse:
+    for (int i = 0; i < arr_data.arr_size; i++) {
+        array_size = arr_data.count_arr[i]-1;
+        array = arr_data.array[i];
         bubble_sort(array, array_size);
     }
 
-    complete_benchmark();
-
     // Cleanup:
-    free(array);
+    cleanup_array_data(arr_data);
+    complete_benchmark();
     return 0;
 }
