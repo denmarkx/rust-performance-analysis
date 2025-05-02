029600517b69c0cc85825552f34c76559692f4b7
Mark <frozenstranger510@gmail.com>
2025-05-02 16:19:36 -0500

    rust_asm: compile oob as lib

--- rust_asm/out_of_bounds.rs
+++ rust_asm/out_of_bounds.rs
@@ -1,6 +1,6 @@
 #[cfg(usafe)]
 #[inline(never)]
-fn check(array : &Vec<u32>) {
+pub fn check(array : &Vec<u32>) {
     unsafe {
         let _x = *array.get_unchecked(7);
     }
@@ -8,12 +8,7 @@ fn check(array : &Vec<u32>) {
 
 #[cfg(not(usafe))]
 #[inline(never)]
-fn check(array : &Vec<u32>) {
+pub fn check(array : &Vec<u32>) {
     // call    _ZN4core9panicking18panic_bounds_check17h883675bd61368af7E
     let _x = array[0];
-}
-
-fn main() {
-    let arr : Vec<u32> = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
-    check(&arr);
 }
