058e510968c2d87b953762fefd156c3b337977fa
Mark <frozenstranger510@gmail.com>
2025-05-02 16:21:47 -0500

    rust_asm: auto-vectorization example; compile all in rust_asm as libs.

---
+++ rust_asm/auto_vectorization.rs
@@ -0,0 +1,23 @@
+// Explicit example derived from out_of_bounds.rs
+
+#[inline(never)]
+#[cfg(not(vectorization))]
+// no simd instructions.
+pub fn update(data: &Vec<u32>, data2: &Vec<u32>) -> u32 {
+    let mut n = 0u32;
+    for i in 1..data.len() {
+        n = data[i] * data2[i-1];
+    }
+    n
+}
+
+#[inline(never)]
+#[cfg(vectorization)]
+// movdqa
+pub fn update(data: &Vec<u32>, data2: &Vec<u32>) -> u32 {
+    let mut n = 0u32;
+    for i in 1..data.len() {
+        n = unsafe { data.get_unchecked(i) * data2.get_unchecked(i-1) };
+    }
+    n
+}
