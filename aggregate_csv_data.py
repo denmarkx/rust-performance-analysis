ec9dca7e525372d5caea8d803c2eb4b3f179cc62
Mark <frozenstranger510@gmail.com>
2025-05-01 20:54:42 -0500

    aggregate_csv_data: quick script to combine the CSVs from rust/c/cxx dirs.

---
+++ aggregate_csv_data.py
@@ -0,0 +1,80 @@
+"""
+Combines several CSVs generated from rust/src/benchmark.rs and c/benchmark.c
+to a single one to make the import process into Polars easier.
+"""
+from pathlib import Path
+import csv, sys
+
+DATA_TYPE = "bubble"
+WANT_UNSAFE_RUST = True
+
+SORTING_FILES = [
+    "bubble",
+    "insertion",
+    "quick",
+]
+
+MATH_FILES = [
+    "matrix",
+]
+
+csv_file = open(f"{DATA_TYPE}-{sys.platform}.csv", "w+")
+writer = csv.writer(csv_file, lineterminator="\n")
+
+headers = []
+if DATA_TYPE in SORTING_FILES:
+    headers = ["CXX", "C", "RUST"]
+    if WANT_UNSAFE_RUST:
+        headers += ["RUST_RAW", "RUST_OOB"]
+    writer.writerow(headers)
+
+CSV_DATA = {header: [] for header in headers}
+MAX_LEN_ROW = 0
+
+for file in Path(".").rglob("*.csv"):
+    ext = file.parent.name
+    if ext not in ["c", "rust"]:
+        continue
+
+    # Unsafe Rust's CSV names are outputted as: <...>_<RAW|OOB>_UNSAFE.csv.
+    if file.name.startswith(DATA_TYPE):
+        # TODO: unsafe rust files
+        if file.name.endswith("UNSAFE.csv") and not WANT_UNSAFE_RUST:
+            continue
+
+        # associate the header based on the filename.
+        if ext == "c":
+            # no need to do this for C or CXX though.
+            header = "C"
+        elif ext in ("cxx", "cpp"):
+            header = "CXX"
+        else:
+            # for rust, we are suffixed by RAW and OOB if we end with unsafe.
+            header = "RUST"
+            if file.name.endswith("UNSAFE.csv"):
+                header = file.name.split("_")[1]
+
+        # Each file has the headers of TIME_NS, TIME_MS, TIME_S.
+        with open(file, "r") as f:
+            reader = csv.reader(f)
+
+            # though we only really care about seconds.
+            seconds = [float(row[2]) for row in list(reader)[1:]]
+            CSV_DATA[header] = seconds
+
+            if len(seconds) > MAX_LEN_ROW:
+                MAX_LEN_ROW = len(seconds)
+
+# fix up data:
+for header, values in CSV_DATA.items():
+    offset = MAX_LEN_ROW - len(values)
+    # polars will complain if the DF size is not all the same upon read_csv (??)
+    if offset > 0:
+        values += [0] * offset
+
+# write by row thanks to the lovely csvwriter
+csv_values = list(CSV_DATA.values())
+for row_index in range(MAX_LEN_ROW):
+    row = [col[row_index] for col in csv_values]
+    writer.writerow(row)
+csv_file.close()
