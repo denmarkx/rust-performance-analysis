1818fb28909f1105dbdcbd61f3d8bd19d8568bfd
Mark <frozenstranger510@gmail.com>
2025-05-02 17:50:03 -0500

    cxx: dirty conversion of malloc'd array -> cxx style array.

--- cxx/main.cxx
+++ cxx/main.cxx
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <vector>
 
 // No need to rewrite benchmark and util.c,
 // but we'll just end up rewriting the sorting/math funcs.
@@ -10,17 +11,47 @@ extern "C" {
     #include "random_value.c"
 }
 
+#include "sorting/insertion.cxx"
+
 // Algorithm -> Function "router"
 typedef struct AlgoFuncHldr {
-    char* string;
-    void (*func)(int*, int);
+    const char* string;
+    void (*func)(std::vector<int>);
 } AlgoFuncHldr;
 
 AlgoFuncHldr AFRouter [] = {
-    // {"insertion", insertion_sort},
+    {"insertion", insertion_sort},
     // {"quick", start_quick_sort},
     // {"bubble", bubble_sort},
 };
+
+// Minimized version of c/util::ArrayData
+// ..I suppose this doesn't really need to be in a struct atp.
+typedef struct VectorData {
+    std::vector<std::vector<int>> array;
+} VectorData;
+
+/*
+* Converts dynamic C-style arrays to vector.
+* See the comment in the main function as to
+* the reason why I'm doing this.
+*/
+VectorData convert_to_vector(ArrayData arr_data) {
+    // int** array;
+    // int* count_arr;
+    // int arr_size.
+    VectorData vec_data;
+    std::vector<std::vector<int>> prim_vec;
+    for (int i = 0; i < arr_data.arr_size; i++) {
+        std::vector<int> vec;
+        for (int j = 0; j < arr_data.count_arr[i]; j++) {
+            vec.push_back(arr_data.array[i][j]);
+        }
+        prim_vec.push_back(vec);
+    }
+    vec_data.array = prim_vec;
+    return vec_data;
+}
 
 
 // ..we can technically maybe sort of perhaps do extern main (with slight changes)
@@ -45,6 +76,13 @@ int main(int argc, char* argv[]) {
         );
     }
 
+    // While you can (and for embedded systems, probably should) use malloc
+    // std::vector is still a highlight of c++ so we're using it here.
+    // obviously there's overhead in that, but it's mainly for a comparison
+    // of c++ features vs. rust.
+    VectorData vec_data = convert_to_vector(arr_data);
+    cleanup_array_data(arr_data);
+
     // algorithm router:
     int f_got_algorithm = 0;
     for (
@@ -55,8 +93,8 @@ int main(int argc, char* argv[]) {
         if (strcmp(router->string, args.algorithm) == 0) {
             // This assumes the parameter types of our func.
             printf("Algorithm: %s\n", args.algorithm);
-            for (unsigned int i = 0; i < arr_data.arr_size; i++) {
-                (*router->func)(arr_data.array[i], arr_data.count_arr[i]-1);
+            for (unsigned int i = 0; i < vec_data.array.size(); i++) {
+                (*router->func)(vec_data.array[i]);
                 f_got_algorithm  = 1;
             }
         }
@@ -72,8 +110,7 @@ int main(int argc, char* argv[]) {
     }
 
     // cleanup
-    cleanup_array_data(arr_data);
-    // complete_benchmark(args.algorithm);
+    complete_benchmark(args.algorithm);
 
     return 0;
 }
